0cc1d965e1671559cbf79fc4b09da4c759822680
Paweł Kukliński <paffcio314@gmail.com>
2020-11-12 21:56:48 +0100

    Add tests for making winning moves

--- tests/test_game.py
+++ tests/test_game.py
@@ -2,7 +2,7 @@ import time
 import pytest
 from checkers.game import Game
 from checkers.player import Player
-from checkers.board import Move
+from checkers.board import Move, CheckersBoard
 from checkers.game import SECS
 
 
@@ -33,11 +33,16 @@ def test_player_can_not_make_to_moves_in_a_row():
 
 
 def test_player_1_runs_out_of_time():
+    # given:
     player_1 = Player("Alice")
     player_2 = Player("Bob")
     g = Game(player_1, player_2, playing_time=1e-16)
+
+    # when:
     time.sleep(2e-16 * SECS)
     winner = g.move(Move(start=(3, "A"), end=(4, "B")))
+
+    # then:
     assert winner.player is player_2
 
 
@@ -55,3 +60,90 @@ def test_player_2_runs_out_of_time():
 
     # then:
     assert winner.player is player_1
+
+
+player_1_win_data = [
+    (
+        """
+- - 
+ -o-
+- -x
+ - -
+""",
+        4,
+        Move(start=(2, "D"), end=(4, "B")),
+    ),
+    (
+        """
+- - -
+ - - 
+-o-o-
+ - -x
+- - -
+""",
+        5,
+        Move(start=(2, "E"), end=(2, "A")),
+    ),
+]
+
+
+@pytest.mark.parametrize("board,dim,move", player_1_win_data)
+def test_player_1_wins_after_winning_move(board, dim, move):
+    # given:
+    player_1 = Player("Alice")
+    player_2 = Player("Bob")
+    board = CheckersBoard.from_ascii(board, dim=dim)
+    g = Game(player_1, player_2, board=board)
+
+    # when:
+    winner = g.move(move)
+
+    # then:
+    assert winner.player is player_1
+
+
+player_2_win_data = [
+    (
+        """
+-o- 
+ - -
+- -x
+ - -
+""",
+        4,
+        [
+            Move(start=(2, "D"), end=(3, "C")),
+            Move(start=(4, "B"), end=(2, "D")),
+        ],
+    ),
+    (
+        """
+- - -
+ - -o
+-x- -
+ - -x
+- - -
+""",
+        5,
+        [
+            Move(start=(2, "E"), end=(3, "D")),
+            Move(start=(4, "E"), end=(4, "A")),
+        ],
+    ),
+]
+
+
+@pytest.mark.parametrize("board,dim,moves", player_2_win_data)
+def test_player_2_wins_after_winning_move(board, dim, moves):
+    # given:
+    player_1 = Player("Alice")
+    player_2 = Player("Bob")
+    board = CheckersBoard.from_ascii(board, dim=dim)
+    g = Game(player_1, player_2, board=board)
+
+    # when:
+    for move in moves:
+        winner = g.move(move)
+
+    # then:
+    assert winner.player is player_2
