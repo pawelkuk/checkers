ce528529b8b5de8b0c7cbc0b519defd167ffe9ee
Paweł Kukliński <paffcio314@gmail.com>
2020-11-28 20:58:49 +0100

    Add init board state

--- frontend/src/App/index.js
+++ frontend/src/App/index.js
@@ -1,14 +1,26 @@
 import { useState } from "react";
 import Board from "../Board/index";
 
+function idx2piece(idx) {
+  if (0 < idx && idx <= 12) return "black";
+  if (12 < idx && idx <= 20) return null;
+  if (20 < idx && idx <= 32) return "white";
+}
+
 function App() {
-  const [piecePosition, setPiecePosition] = useState([0, 0]);
+  const allAccessibleFields = 33;
+  const arr = Array.from({ length: allAccessibleFields }, (_, index) => [
+    index,
+    idx2piece(index),
+  ]);
+  const initBoard = Object.fromEntries(arr);
+  const [piecePositions, setPiecePosition] = useState([0, 0]);
   const onChange = (position) => {
     setPiecePosition(position);
   };
   return (
     <div>
-      <Board position={piecePosition} onChange={onChange} />
+      <Board position={piecePositions} onChange={onChange} />
     </div>
   );
 }
